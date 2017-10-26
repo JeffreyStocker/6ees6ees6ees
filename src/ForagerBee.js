b10551853ec62c2406edc31ba025272e923af379
Jeff <jeffreyRstocker@gmail.com>
2017-10-25 22:29:29 -0700

    Passed all tests

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
+class ForagerBee extends Bee {
+  constructor() {
+    super();
+    this.age = 10;
+    this.job = 'find pollen';
+    this.treasureChest = [];
+    this.canFly = true;
+  }
+  forage (treasure){
+    this.treasureChest.push (treasure);
+  }
   // TODO..
 };
